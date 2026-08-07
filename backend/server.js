496d368bd05ad8fd76ce6c53a767fdbd3ed57e8f
mannsvijay <mnvj69@gmail.com>
2026-08-07 11:27:34 +0530

    Connect to MongoDB in backend server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb://admin:password@localhost:27017/user-account?authSource=admin"
+)
+.then(() => console.log("✅ Connected to MongoDB"))
+.catch(err => console.log("MongoDB Error:", err));
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
